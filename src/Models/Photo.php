31b58af478765863061d469cdaa067716b22722c
derweili <jw@derweili.de>
2017-11-16 20:08:34 +0100

    photo data model added

---
+++ src/Models/Photo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace \Derweili\Instabot\;
+
+/**
+ * Photo Class
+ */
+class Photo
+{
+
+  public $id;
+
+  public $original_id;
+
+  public $image_url;
+
+  public $source_name;
+
+  public $topic;
+
+  public $date;
+
+  public $data;
+
+  public $keyword;
+
+  public $description;
+
+}
