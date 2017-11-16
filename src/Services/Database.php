7aeaf5d1c455e5f7f675463106611eae10d3200c
derweili <jw@derweili.de>
2017-11-16 20:48:16 +0100

    fix: Database service issues

--- src/Services/Database.php
+++ src/Services/Database.php
@@ -1,7 +1,7 @@
 <?php
 namespace Derweili\Instabot\Services;
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\ServiceAccount;
+use \Kreait\Firebase\Factory;
+use \Kreait\Firebase\ServiceAccount;
 
 /**
  * Service for database connection
@@ -10,17 +10,19 @@ class Database
 {
 
   static public $firebase;
+  static public $database;
 
   static public function setup( $path_to_config_json ){
     $serviceAccount = ServiceAccount::fromJsonFile( $path_to_config_json );
     self::$firebase = (new Factory)
         ->withServiceAccount( $serviceAccount )
         ->create();
+    self::$database = self::$firebase->getDatabase();
   }
 
 
   static public function store_image( $image ){
-    return $database->getReference('images/' . $image->id )
+    return self::$database->getReference('images/' . $image->id )
     ->set( $image );
   }
 
